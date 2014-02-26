23e9d9826084599259b1a3885713a551f6789519
joshvillahermosa <joshvillahermosa@gmail.com>
2014-02-26 17:11:12 -0600

    Cleaned out past code to work with new plate

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('#SwipeCarousel').hammer().on("dragright", function(event){
-		$('#SwipeCarousel').carousel('prev');
-	});
-
-	$('#SwipeCarousel').hammer().on("dragleft", function(event){
-		$('#SwipeCarousel').carousel('next');
-	});
-});
